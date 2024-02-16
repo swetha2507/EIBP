0be557dff40e85f4fb04541167869a975491d2f1
Kriti Goel <143528668+kg8849@users.noreply.github.com>
2024-02-16 11:31:00 -0500

    Delete FABRIC-EIBP directory

--- FABRIC-EIBP/Fabric-EIBP-Jupyter_scripts/remote_scripts/init_mtp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# $1 = name of node
-
-# Prep for tmux
-echo "tmux pipe-pane -o 'cat >>~/EIBP_$1.log'" > ~/tmux_start_logging.sh
-sudo chmod 777 ~/tmux_start_logging.sh
-echo "set -g remain-on-exit on" > ~/.tmux.conf
-echo "set-hook -g after-new-session 'run ~/tmux_start_logging.sh'" >> ~/.tmux.conf
